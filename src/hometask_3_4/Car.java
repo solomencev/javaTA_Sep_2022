dedd8b6ee8c8eb5fd54ec796ee16ab451f7de886
iurii solomentsev <emirateskos@gmail.com>
2022-09-18 08:42:03 +0400

    Create class Car, add to cars

--- src/hometask_3_4/Car.java
+++ src/hometask_3_4/Car.java
@@ -3,14 +3,30 @@ package hometask_3_4;
 public class Car {
     String color;
     String gear;
+    String manufacturer;
+    double mileage;
+    int doors;
 
 }
 
 class CarTest {
     public static void main(String[] args) {
-        Car car1 = new Car();
-        car1.color = "Red";
-        car1.gear = "auto";
-        System.out.println(car1.color + " " + car1.gear);
+        Car mazda3 = new Car();
+        mazda3.color = "red";
+        mazda3.gear = "auto";
+        mazda3.manufacturer = "Mazda";
+        mazda3.mileage = 145.567;
+        mazda3.doors = 3;
+        System.out.println("Mazda 3 has the following parameters: " + "color: " + mazda3.color + "; gear: " + mazda3.gear
+        + "; manufacturer: " + mazda3.manufacturer + "; mileage: " + mazda3.mileage + "; doors: " + mazda3.doors);
+
+        Car golf6 = new Car();
+        golf6.color = "white";
+        golf6.gear = "manual";
+        golf6.manufacturer = "Volkswagen";
+        golf6.mileage = 170.5;
+        golf6.doors = 5;
+        System.out.println("Golf 6 has the following parameters: " + "color: " + golf6.color + "; gear: " + golf6.gear
+                + "; manufacturer: " + golf6.manufacturer + "; mileage: " + golf6.mileage + "; doors: " + golf6.doors);
     }
 }
