c71924d982f07aaa3496a44125b2c558d14a1f23
iurii solomentsev <emirateskos@gmail.com>
2022-09-18 19:21:16 +0400

    Passing arguments when creating a class instance

--- src/hometask_3_4/Car.java
+++ src/hometask_3_4/Car.java
@@ -31,16 +31,18 @@ public class Car {
 
     int increaseSpeed (int pace) {
         speed+=pace;
+        System.out.println("Speed is increased on: " + pace);
         return  speed;
     }
 
     int slowDown (int pace) {
         speed-=pace;
+        System.out.println("Speed reduced by: " + pace);
         return speed;
     }
 
     void showInfo(){
-        System.out.println("Mazda 3 has the following parameters: " + "color: " + color + "; gear: " + gear
+        System.out.println("The create car has the following parameters: " + "color: " + color + "; gear: " + gear
                 + "; manufacturer: " + manufacturer + "; mileage: " + mileage + "; doors: " + doors + " current speed: " + speed);
     }
 
@@ -48,18 +50,14 @@ public class Car {
 
 class CarTest {
     public static void main(String[] args) {
-        Car mazda3 = new Car();
-       mazda3.color = "white";
-       mazda3.doors = 5;
-       mazda3.mileage = 100000;
-       mazda3.gear = "manual";
-       mazda3.manufacturer = "Mazda";
-       mazda3.speed = 90;
-       mazda3.showInfo();
-       mazda3.increaseSpeed(10);
-       mazda3.showInfo();
-       mazda3.slowDown(100);
-       mazda3.showInfo();
+        Car mazda3 = new Car("white", "automation", "Mazda", 150.000, 5, 90);
+        mazda3.showInfo();
+        mazda3.increaseSpeed(10);
+        mazda3.showInfo();
+        mazda3.slowDown(90);
+        mazda3.showInfo();
+
+
 
     }
 }
