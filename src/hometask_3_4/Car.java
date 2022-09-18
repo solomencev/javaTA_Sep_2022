3abd97dcbd9ca42ceb5c2b56ff8668e717cd85c2
iurii solomentsev <emirateskos@gmail.com>
2022-09-18 17:37:37 +0400

    Updated an existing car. Added methods: increaseSpeed, slowDown, showInfo.

--- src/hometask_3_4/Car.java
+++ src/hometask_3_4/Car.java
@@ -1,32 +1,47 @@
 package hometask_3_4;
 
 public class Car {
+    Car(){
+        System.out.println("Car is created");
+    }
     String color;
     String gear;
     String manufacturer;
     double mileage;
     int doors;
+    int speed;
+
+    int increaseSpeed (int pace) {
+        speed+=pace;
+        return  speed;
+    }
+
+    int slowDown (int pace) {
+        speed-=pace;
+        return speed;
+    }
+
+    void showInfo(){
+        System.out.println("Mazda 3 has the following parameters: " + "color: " + color + "; gear: " + gear
+                + "; manufacturer: " + manufacturer + "; mileage: " + mileage + "; doors: " + doors + " current speed: " + speed);
+    }
 
 }
 
 class CarTest {
     public static void main(String[] args) {
         Car mazda3 = new Car();
-        mazda3.color = "red";
-        mazda3.gear = "auto";
-        mazda3.manufacturer = "Mazda";
-        mazda3.mileage = 145.567;
-        mazda3.doors = 3;
-        System.out.println("Mazda 3 has the following parameters: " + "color: " + mazda3.color + "; gear: " + mazda3.gear
-        + "; manufacturer: " + mazda3.manufacturer + "; mileage: " + mazda3.mileage + "; doors: " + mazda3.doors);
+       mazda3.color = "white";
+       mazda3.doors = 5;
+       mazda3.mileage = 100000;
+       mazda3.gear = "manual";
+       mazda3.manufacturer = "Mazda";
+       mazda3.speed = 90;
+       mazda3.showInfo();
+       mazda3.increaseSpeed(10);
+       mazda3.showInfo();
+       mazda3.slowDown(100);
+       mazda3.showInfo();
 
-        Car golf6 = new Car();
-        golf6.color = "white";
-        golf6.gear = "manual";
-        golf6.manufacturer = "Volkswagen";
-        golf6.mileage = 170.5;
-        golf6.doors = 5;
-        System.out.println("Golf 6 has the following parameters: " + "color: " + golf6.color + "; gear: " + golf6.gear
-                + "; manufacturer: " + golf6.manufacturer + "; mileage: " + golf6.mileage + "; doors: " + golf6.doors);
     }
 }
