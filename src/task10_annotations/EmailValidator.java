9958631d8202d2311ca9e3831c72ad181de39440
iurii solomentsev <emirateskos@gmail.com>
2022-11-19 14:19:19 +0400

    Added EmailValidator class

---
+++ src/task10_annotations/EmailValidator.java
@@ -0,0 +1,21 @@
+package task10_annotations;
+
+import java.util.InputMismatchException;
+import java.util.regex.Pattern;
+
+public class EmailValidator {
+    public EmailValidator(String email) {
+        try {
+            Pattern emailCheck = Pattern.compile("^\\w*@\\w*\\.[a-z]{1,3}$");
+            boolean emailCheckResult = emailCheck.matcher(email).find();
+            System.out.println(emailCheckResult);
+            if (emailCheckResult == true) {
+                System.out.println("E-mail Validated");
+            } else {
+                System.out.println("Incorrect E-mail");
+
+            }
+        }
+
+    }
+}
